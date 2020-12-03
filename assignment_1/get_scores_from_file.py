002cd3efc30f8c9e1be860579cf1620b8b4f54a4
Martijn <m.p.devries@studente.utwente.nl>
2020-12-03 14:52:56 +0100

    Genuine scores added

--- assignment_1/get_scores_from_file.py
+++ assignment_1/get_scores_from_file.py
@@ -1,6 +1,19 @@
 from matplotlib import pyplot
 import numpy
 
+
+def scores(id, S):
+    [np, nt] = S.shape
+
+    result = numpy.zeros(S.shape)
+
+    for i in range(np):
+        for j in range(i):
+            if id[i] == id[j]:
+                result[i, j] = 1
+    return result
+
+
 if __name__ == "__main__":
     Id = numpy.loadtxt('id.txt')
     S = numpy.loadtxt('scorematrix.txt')
@@ -8,8 +21,12 @@ if __name__ == "__main__":
     nId = Id.max()
     Entries = numpy.arange(1, np + 1)
 
+    gen_imp = scores(Id, S)
+
     print('Size of score matrix {0} x {1}'.format(np, nt))
     print('Number of identities {0}'.format(nId))
+    print('Number of genuine scores: {0}'.format(numpy.count_nonzero(gen_imp)))
+    print('Number of impostor scores: {0}'.format((np * nt) - numpy.count_nonzero(gen_imp)))
 
     pyplot.plot(Entries, Id)
     pyplot.xlabel('Entry')
@@ -18,4 +35,7 @@ if __name__ == "__main__":
     pyplot.show()
 
     pyplot.imshow(S)
+    pyplot.show()
+
+    pyplot.imshow(gen_imp)
     pyplot.show()
