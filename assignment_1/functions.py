d5aa96fc813f3f4b6e19fe2ed3973a5c40c30cff
Marco Boom <marco.boom@outlook.com>
2021-01-27 12:12:53 +0100

    fixed match rate bug

--- assignment_1/functions.py
+++ assignment_1/functions.py
@@ -13,16 +13,26 @@ def classify_scores(S, id):
 
     return result
 
-def get_match_rate(scores, t, greater = True):
-    thresholded_scores = get_threshold_scores(scores, t, greater)
+def filter_matrix(S):
+    [np, _] = S.shape
+    result = numpy.full(S.shape, False)
+
+    for i in range(np):
+        for j in range(i):
+            result[i, j] = True
+
+    return result
+
+def get_match_rate(filter, scores, t, greater = True):
+    thresholded_scores = get_threshold_scores(filter, scores, t, greater)
     thresholded_scores_count = numpy.count_nonzero(thresholded_scores)
     scores_count = numpy.count_nonzero(scores)
     return thresholded_scores_count / scores_count
 
-def get_threshold_scores(S, t, greater = True):
+def get_threshold_scores(filter, S, t, greater = True):
     result = numpy.greater(S, t)
 
     if not greater:
         result = numpy.invert(result)
 
-    return result
+    return result & filter
