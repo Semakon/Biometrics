b88a034d427e9a90230850e8842d6aedcae069c8
Martijn <m.p.devries@studente.utwente.nl>
2021-01-28 14:56:34 +0100

    Added histogram

--- assignment_1/get_scores_from_file.py
+++ assignment_1/get_scores_from_file.py
@@ -39,11 +39,16 @@ def calc_fmr_fnmr(Id, S):
     gen = []
     imp = []
     for i in range(len(gen_imp)):
-        for j in range(len(gen_imp[i])):
+        for j in range(i):
             if gen_imp[i, j] == 0:
                 imp.append(S[i, j])
             else:
                 gen.append(S[i, j])
+
+    # Save gen and imp to file
+    numpy.savetxt('genuine.txt', gen)
+    numpy.savetxt('impostor.txt', imp)
+
     # Determine min and max threshold
     min_s = numpy.min(S)
     max_s = numpy.max(S)
@@ -53,6 +58,8 @@ def calc_fmr_fnmr(Id, S):
     # print('Average genuine score: {0}'.format(numpy.mean(gen)))
     # print('Average impostor score: {0}'.format(numpy.mean(imp)))
     # print()
+    # print('Number of genuine scores: {0}'.format(len(gen)))
+    # print('Number of impostor scores: {0}'.format(len(imp)))
 
     # Determine FMR as a function of decision threshold
     thresholds = numpy.arange(int(min_s), int(max_s), 1)
@@ -70,10 +77,18 @@ def calc_fmr_fnmr(Id, S):
     numpy.savetxt('fmr_t.txt', fmr_t)
     numpy.savetxt('fnmr_t.txt', fnmr_t)
 
-    return thresholds, fmr_t, fnmr_t
+    return gen, imp, thresholds, fmr_t, fnmr_t
 
 
-def show_graphs(thresholds, fmr_t, fnmr_t):
+def show_graphs(gen, imp, thresholds, fmr_t, fnmr_t):
+    # Plot the genuine and impostor scores in a histogram
+    n_bins = 200
+    fig, axs = pyplot.subplots(1, 2, sharey=True, tight_layout=True)
+    axs[0].hist(gen, bins=n_bins)
+    axs[1].hist(imp, bins=n_bins)
+    # pyplot.title('Histogram of Genuine (left) and Impostor (right) scores (bin size is 200)')
+    pyplot.show()
+
     # Plot FMR and FNMR on same graph
     pyplot.plot(thresholds, fmr_t, label='FMR')
     pyplot.plot(thresholds, fnmr_t, label='FNMR')
@@ -107,19 +122,21 @@ if __name__ == "__main__":
 
     # print('Size of score matrix: {0} x {1}'.format(np, nt))
     # print('Number of identities: {0}'.format(nId))
-    # print('Number of genuine scores: {0}'.format(len(gen)))
-    # print('Number of impostor scores: {0}'.format(len(imp)))
-    # print()
 
-    # Get FMR(t), FNMR(t), and the thresholds
-    if path.exists('thresholds.txt') and path.exists('fmr_t.txt') and path.exists('fnmr_t.txt'):
-        # Get FMR(t) and FNMR(t) from file
+    # Get data
+    if path.exists('genuine.txt') and path.exists('impostor.txt') \
+            and path.exists('thresholds.txt') and path.exists('fmr_t.txt') \
+            and path.exists('fnmr_t.txt'):
+        # Get data from file
+        gen = numpy.loadtxt('genuine.txt')
+        imp = numpy.loadtxt('impostor.txt')
+
         thresholds = numpy.loadtxt('thresholds.txt')
         fmr_t = numpy.loadtxt('fmr_t.txt')
         fnmr_t = numpy.loadtxt('fnmr_t.txt')
     else:
         # Calculate FMR(t) and FNMR(t)
-        thresholds, fmr_t, fnmr_t = calc_fmr_fnmr(Id, S)
+        gen, imp, thresholds, fmr_t, fnmr_t = calc_fmr_fnmr(Id, S)
 
-    # Plot the FMR, FNMR, DET curve, and ROC curve
-    show_graphs(thresholds, fmr_t, fnmr_t)
+    # Plot the histogram, FMR, FNMR, DET curve, and ROC curve
+    show_graphs(gen, imp, thresholds, fmr_t, fnmr_t)
